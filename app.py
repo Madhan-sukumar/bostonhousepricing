79c5d6253457293cadfc17298194c83c1280bda0
Madhan <madhanbalasukumar@gmail.com>
2023-04-12 20:56:21 +0100

    The Web App is Ready

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ scalar=pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 
-#to give some input
+#to give some input and test the api on POSTMAN
 @app.route('/predict_api',methods =['POST'])
 def predict_api():
-    #converts the input data into json and store it on the data variable
+    #converts the input data into json and store it on the data variable using request object
     data = request.json['data']
     print(data)
 
@@ -30,8 +30,26 @@ def predict_api():
 
     #fitting the given data
     output = regmodel.predict(new_data)
+
+    #since it return 2d array and need 1st value
     print(output[0])
     return jsonify(output[0])
+
+#to capture and transform all values in front end application
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    #to capture all the values using form in html and request object. convert into as list using list comprehension
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    
+    #since it is a 2d array and need 1st value
+    output = regmodel.predict(final_input)[0]
+
+    #using render template, replace the predicted value in the place holder "prediction_text" on home.html  
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 
 if __name__ =="__main__":
     app.run(debug = True)
